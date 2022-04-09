66eb171757bcc353fda305e328cd0a172fb49b84
Maryam Masoumi <mmasoumi15@gmail.com>
2022-04-09 22:03:38 +0430

    first commit

---
+++ app/src/main/java/project/ynp/composesample/MainActivity.kt
@@ -0,0 +1,22 @@
+package project.ynp.composesample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import project.ynp.composesample.ui.theme.ComposeSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello world!")
+        }
+    }
+}
