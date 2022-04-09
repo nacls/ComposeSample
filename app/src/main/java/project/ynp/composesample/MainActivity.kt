0d8f42f74438a649630de039bddaf6ade5c03e82
Maryam Masoumi <mmasoumi15@gmail.com>
2022-04-09 22:43:43 +0430

    Added profile header to main activity

--- app/src/main/java/project/ynp/composesample/MainActivity.kt
+++ app/src/main/java/project/ynp/composesample/MainActivity.kt
@@ -3,20 +3,58 @@ package project.ynp.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import project.ynp.composesample.ui.theme.ComposeSampleTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+                MessageCard(Profile("Maryam Masoumi", "I'm very enthusiastic about Kotlin and Jetpack Compose!"))
         }
     }
+}
+
+data class Profile(val name: String, val description: String)
+
+@Composable
+fun MessageCard(msg: Profile) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column {
+            Text(
+                text = msg.name,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(text = msg.description)
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Profile("Jane Doe", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
